a4cf8d783eb46491a64bffb5cec8ee931f4eb0c5
Philip Schröder <philip.schroeder@shs-viveon.com>
2016-08-16 23:15:21 +0200

    SumOfNumbers implemented

--- codewars/SumOfNumbers/SumOfNumbersKata.cs
+++ codewars/SumOfNumbers/SumOfNumbersKata.cs
@@ -10,8 +10,13 @@ namespace codewars.SumOfNumbers
     {
         public static int GetSum(int a, int b)
         {
-            //Good Luck!
-            return 0;
+            int sum = 0;
+            for (int i = Math.Min(a, b); i <= Math.Max(a, b); i++)
+            {
+                sum += i;
+            }
+
+            return sum;
         }
     }
 }

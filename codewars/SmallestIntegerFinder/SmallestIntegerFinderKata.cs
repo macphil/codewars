ed0be54bb8e9c814f5e0bf759aa4fe7b768d7807
Philip Schröder <philip.schroeder@shs-viveon.com>
2016-08-16 22:23:40 +0200

    SmallestIntegerFinder: implemented

---
+++ codewars/SmallestIntegerFinder/SmallestIntegerFinderKata.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace codewars.SmallestIntegerFinder
+{
+    public class SmallestIntegerFinderKata
+    {
+        public static int FindSmallestInt(int[] args)
+        {
+            return args.Min();
+        }
+    }
+}
---
+++ codewars/SmallestIntegerFinder/SmallestIntegerFinderTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace codewars.SmallestIntegerFinder
+{
+    public class SmallestIntegerFinderTests
+    {
+        [TestCase(new int[] { 78, 56, 232, 12, 11, 43 }, 11)]
+        [TestCase(new int[] { 78, 56, -2, 12, 8, -33 }, -33)]
+        public static void FixedTest(int[] args, int expected)
+        {
+            Assert.AreEqual(expected, SmallestIntegerFinderKata.FindSmallestInt(args));
+        }
+    }
+}

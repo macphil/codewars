6a9ffb48ea3115390dcfb78f32f194d83a85ae67
Philip Schröder <philip.schroeder@shs-viveon.com>
2016-08-16 23:02:57 +0200

    SumOfNumbers added

---
+++ codewars/SumOfNumbers/SumOfNumbersKata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codewars.SumOfNumbers
+{
+    internal class SumOfNumbersKata
+    {
+        public static int GetSum(int a, int b)
+        {
+            //Good Luck!
+            return 0;
+        }
+    }
+}
---
+++ codewars/SumOfNumbers/SumOfNumbersTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace codewars.SumOfNumbers
+{
+    public class SumOfNumbersTests
+    {
+        [Test]
+        [TestCase(1, 0, 1)]   // 1 + 0 = 1
+        [TestCase(1, 2, 3)]   // 1 + 2 = 3
+        [TestCase(0, 1, 1)]   // 0 + 1 = 1
+        [TestCase(1, 1, 1)]   // 1 Since both are same
+        [TestCase(-1, 0, -1)] // -1 + 0 = -1
+        [TestCase(-1, 2, 2)]  // -1 + 0 + 1 + 2 = 2
+        public void SumOfNumbersTest_AreEqual(int i, int j, int expected)
+        {
+            Assert.AreEqual(expected, SumOfNumbersKata.GetSum(i, j));
+        }
+    }
+}

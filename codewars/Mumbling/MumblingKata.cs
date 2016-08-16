52e08840891449b2a06007cdc7124dd883330e5d
Philip Schröder <philip.schroeder@shs-viveon.com>
2016-08-16 17:33:55 +0200

    mumbling added

---
+++ codewars/Mumbling/MumblingKata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codewars.Mumbling
+{
+    internal class MumblingKata
+    {
+        public static String Accum(string s)
+        {
+            return s;
+        }
+    }
+}
---
+++ codewars/Mumbling/MumblingTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace codewars.Mumbling
+{
+    public class MumblingTests
+    {
+        [Test]
+        [TestCase("ZpglnRxqenU", "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu")]
+        [TestCase("NyffsGeyylB", "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb")]
+        [TestCase("MjtkuBovqrU", "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu")]
+        [TestCase("EvidjUnokmM", "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm")]
+        [TestCase("HbideVbxncC", "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc")]
+        public void MumblingTest(string actual, string expected)
+        {
+            Assert.AreEqual(expected, MumblingKata.Accum(actual));
+        }
+    }
+}

7c7af8bf736e1d6605260109f2b657b69cc527a7
Philip Schröder <philip.schroeder@shs-viveon.com>
2016-08-19 14:17:58 +0200

    mumbling renamed

--- codewars/Mumbling/MumblingKata.cs
+++ codewars/Mumbling/MumblingKata.cs
@@ -5,7 +5,7 @@ namespace codewars.Mumbling
 {
     internal class MumblingKata
     {
-        public static String Accum(string s)
+        public static String Mumble(string s)
         {
             return string.Join("-", s.Select((x, i) => char.ToUpper(x) + new string(char.ToLower(x), i)));
         }
--- codewars/Mumbling/MumblingTests.cs
+++ codewars/Mumbling/MumblingTests.cs
@@ -5,14 +5,18 @@ namespace codewars.Mumbling
     public class MumblingTests
     {
         [Test]
-        [TestCase("ZpglnRxqenU", "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu")]
-        [TestCase("NyffsGeyylB", "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb")]
-        [TestCase("MjtkuBovqrU", "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu")]
-        [TestCase("EvidjUnokmM", "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm")]
-        [TestCase("HbideVbxncC", "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc")]
-        public void MumblingTest(string actual, string expected)
+        [TestCase("ZpglnRxqenU", ExpectedResult = "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu")]
+        [TestCase("NyffsGeyylB", ExpectedResult = "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb")]
+        [TestCase("MjtkuBovqrU", ExpectedResult = "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu")]
+        [TestCase("EvidjUnokmM", ExpectedResult = "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm")]
+        [TestCase("HbideVbxncC", ExpectedResult = "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc")]
+        [TestCase("Schrööööder", ExpectedResult = "S-Cc-Hhh-Rrrr-Ööööö-Öööööö-Ööööööö-Öööööööö-Ddddddddd-Eeeeeeeeee-Rrrrrrrrrrr")]
+        [TestCase("Philip", ExpectedResult = "P-Hh-Iii-Llll-Iiiii-Pppppp")]
+        [TestCase("Jawhar", ExpectedResult = "J-Aa-Www-Hhhh-Aaaaa-Rrrrrr")]
+        [TestCase(@"@.ß*1", ExpectedResult = "@-..-ßßß-****-11111")]
+        public string MumblingTest(string actual)
         {
-            Assert.AreEqual(expected, MumblingKata.Accum(actual));
+            return MumblingKata.Mumble(actual);
         }
     }
 }

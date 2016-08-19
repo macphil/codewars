0e8b74c9e52b9f5663c4711b191a1d3bec3afa8d
Philip Schröder <philip.schroeder@shs-viveon.com>
2016-08-19 15:43:10 +0200

    ToRomanNumerals: introduce dictionary

--- codewars/ToRomanNumerals/ToRomanNumeralsKata.cs
+++ codewars/ToRomanNumerals/ToRomanNumeralsKata.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
 
 namespace codewars.ToRomanNumerals
 {
@@ -14,24 +16,45 @@ namespace codewars.ToRomanNumerals
 
             var romanNumber = string.Empty;
 
-            romanNumber += new string('M', Convert.ToInt32(value / 1000));
-            value = value % 1000;
-            romanNumber += new string('D', Convert.ToInt32(value / 500));
-            value = value % 500;
-            romanNumber += new string('C', Convert.ToInt32(value / 100));
-            value = value % 100;
-            romanNumber += new string('L', Convert.ToInt32(value / 50));
-            value = value % 50;
-            romanNumber += new string('X', Convert.ToInt32(value / 10));
-            value = value % 10;
-            romanNumber += new string('V', Convert.ToInt32(value / 5));
-            value = value % 5;
-            romanNumber += new string('I', Convert.ToInt32(value / 1));
+            foreach (KeyValuePair<int, string> romanSign in RomanSigns.Reverse())
+            {
+                var anz = Convert.ToInt32(value / romanSign.Key);
+                if (anz == 0)
+                {
+                    continue;
+                }
 
-            romanNumber = HandleNine(romanNumber);
-            romanNumber = HandleFour(romanNumber);
+                romanNumber += (romanSign.Value.Length == 1) ?
+                    new string(romanSign.Value.ToCharArray()[0], anz)
+                    : romanSign.Value;
+
+                value = value % romanSign.Key;
+            }
 
             return romanNumber;
+        }
+
+        private static Dictionary<int, string> RomanSigns
+        {
+            get
+            {
+                return new Dictionary<int, string>
+                {
+                    {1, "I"},
+                    {4, "IV"},
+                    {5, "V"},
+                    {9, "IX"},
+                    {10, "X"},
+                    {40, "XL"},
+                    {50, "L"},
+                    {90, "XC"},
+                    {100, "C"},
+                    {400, "CD"},
+                    {500, "D"},
+                    {900, "CM"},
+                    {1000, "M"}
+                };
+            }
         }
 
         private static string HandleFour(string romanNumber)

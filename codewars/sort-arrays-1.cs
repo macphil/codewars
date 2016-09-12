7ba3af49b65922c7d50c70eafcfdab58c0509c32
Philip Schröder <philip.schroeder@shs-viveon.com>
2016-09-12 21:15:30 +0200

    submitted solution

--- codewars/sort-arrays-1.cs
+++ codewars/sort-arrays-1.cs
@@ -15,39 +15,7 @@ namespace codewars
 
         private string[] SortMe(string[] names)
         {
-            var numbers = new List<int>();
-            foreach (string name in names.Where(x => names.Contains(x)))
-            {
-                numbers.Add(Numerals.First(x => x.Value == name).Key);
-            }
-
-            var numeralsList = new List<string>();
-            foreach (int number in numbers)
-            {
-                numeralsList.Add(Numerals[number]);
-            }
-
-            return numeralsList.ToArray();
-        }
-
-        public Dictionary<int, string> Numerals
-        {
-            get
-            {
-                return new Dictionary<int, string>
-                {
-                    {1, "one"},
-                    {2, "two"},
-                    {3, "three"},
-                    {4, "four"},
-                    {5, "five"},
-                    {6, "six"},
-                    {7, "seven"},
-                    {8, "eight"},
-                    {9, "nine"},
-                    {10, "ten"}
-                };
-            }
+            return names.OrderBy(x => x).ToArray();
         }
     }
 }

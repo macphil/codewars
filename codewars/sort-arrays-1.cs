124fff9354bf1cea2ac33529b5885c38245edaee
Philip Schröder <philip.schroeder@shs-viveon.com>
2016-09-12 21:12:02 +0200

    one, two, three

--- codewars/sort-arrays-1.cs
+++ codewars/sort-arrays-1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace codewars
@@ -13,7 +15,39 @@ namespace codewars
 
         private string[] SortMe(string[] names)
         {
-            return names;
+            var numbers = new List<int>();
+            foreach (string name in names.Where(x => names.Contains(x)))
+            {
+                numbers.Add(Numerals.First(x => x.Value == name).Key);
+            }
+
+            var numeralsList = new List<string>();
+            foreach (int number in numbers)
+            {
+                numeralsList.Add(Numerals[number]);
+            }
+
+            return numeralsList.ToArray();
+        }
+
+        public Dictionary<int, string> Numerals
+        {
+            get
+            {
+                return new Dictionary<int, string>
+                {
+                    {1, "one"},
+                    {2, "two"},
+                    {3, "three"},
+                    {4, "four"},
+                    {5, "five"},
+                    {6, "six"},
+                    {7, "seven"},
+                    {8, "eight"},
+                    {9, "nine"},
+                    {10, "ten"}
+                };
+            }
         }
     }
 }

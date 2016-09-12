f2c450023add3a04d55a679748ca6907620ebafe
Philip Schröder <philip.schroeder@shs-viveon.com>
2016-09-12 23:03:24 +0200

    submitted

--- codewars/Ipv4Validator.cs
+++ codewars/Ipv4Validator.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using NUnit.Framework;
 
 /* https://www.codewars.com/kata/ipv4-validator/train/csharp
 *
@@ -28,6 +31,14 @@ namespace codewars
     {
         [Test]
         [TestCase("123.456.789.0", false)]
+        [TestCase("1444.23.9", false)]
+        [TestCase("-12.344.43.11", false)]
+        [TestCase("153.500.234.444", false)]
+        [TestCase("153.a.234.444", false)]
+        [TestCase(" 1.2.3.4", false)]
+        [TestCase("1.2.3.4 ", false)]
+        [TestCase("", false)]
+        [TestCase(null, false)]
         [TestCase("127.0.0.1", true)]
         public void IpValidator_AreEqual(string actual, bool expected)
         {
@@ -36,7 +47,18 @@ namespace codewars
 
         public static bool IpValidator(string ip)
         {
-            return false;
+            if (string.IsNullOrWhiteSpace(ip))
+            {
+                return false;
+            }
+            var bytes = ip.Split('.');
+            if (bytes.Length != 4)
+            {
+                return false;
+            }
+
+            byte parsedByte;
+            return bytes.All(b => Byte.TryParse(b, NumberStyles.None, CultureInfo.InvariantCulture, out parsedByte));
         }
     }
 }

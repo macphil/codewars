1f607a47c06da2c2154f321d78d073e6eaf94db8
Philip Schröder <philip.schroeder@shs-viveon.com>
2016-09-13 12:07:39 +0200

    submitted

--- codewars/Highest_Rank_Number _in_an_Array.cs
+++ codewars/Highest_Rank_Number _in_an_Array.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
 
 /// <summary>
 /// https://www.codewars.com/kata/highest-rank-number-in-an-array/train/csharp
@@ -14,16 +16,10 @@ namespace codewars
         [TestCase(new int[] { 12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10 }, ExpectedResult = 3, TestName = "drei")]
         [TestCase(new int[] { 12, 10 }, ExpectedResult = 12, TestName = "vier")]
         [TestCase(new int[] { 12, 10, 8, 12, 7, 6, 4, 10, 12 }, ExpectedResult = 12, TestName = "fuenf")]
+        [TestCase(new int[] { 5, 8, 5, 11, 6, 11, 7, 8, 6 }, ExpectedResult = 11, TestName = "sechs")]
         public int Highest_Rank_Number__in_an_Array_AreEqual(int[] arr)
         {
-            int res = -1;
-            int maxCount = 0;
-            //foreach (int i in arr.Length)
-            //{
-            //    if (arr.arr.GetValue(i))
-            //}
-
-            return res;
+            return arr.GroupBy(x => x).OrderByDescending(x => x.Key).OrderByDescending(g => g.Count()).Select(g => g.Key).First();
         }
     }
 }

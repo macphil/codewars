c0db0d2d3b0c3d7c72323a0d85f7020dcd144da1
Philip Schröder <philip.schroeder@shs-viveon.com>
2016-09-13 12:49:02 +0200

    SplitString

---
+++ codewars/SplitString.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace codewars
+{
+    public class SplitString
+    {
+        [Test]
+        [TestCase("abc", ExpectedResult = new string[] { "ab", "c_" })]
+        [TestCase("abcdef", ExpectedResult = new string[] { "ab", "cd", "ef" })]
+        public string[] SplitString_Test(string actual)
+        {
+            return Solution(actual);
+        }
+
+        public static string[] Solution(string str)
+        {
+            return null;
+        }
+    }
+}

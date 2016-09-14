0f29e0d4ff31b015706ae04d4a6425e9a8b4c79c
Philip Schröder <philip.schroeder@shs-viveon.com>
2016-09-14 21:18:52 +0200

    first approach

--- codewars/NextBiggerNumberKata.cs
+++ codewars/NextBiggerNumberKata.cs
@@ -67,34 +67,49 @@ namespace codewars
 
         [Test]
         [TestCase("123", "456", ExpectedResult = "579")]
+        [TestCase("123", "7", ExpectedResult = "130")]
+        [TestCase("1", "2015", ExpectedResult = "2016")]
+        [TestCase("788", "22", ExpectedResult = "810")]
         [TestCase("", "5", ExpectedResult = "5")]
         [TestCase("712569312664357328695151392", "8100824045303269669937", ExpectedResult = "712577413488402631964821329")]
         [TestCase("50095301248058391139327916261", "81055900096023504197206408605", ExpectedResult = "131151201344081895336534324866")]
-        public string Given123And456Returns579(string a, string b)
+        public string SumStrings_Test(string a, string b)
         {
             return SumStrings(a, b);
         }
 
         private string SumStrings(string a, string b)
         {
-            decimal a_decimal, b_decimal;
-            if (string.IsNullOrWhiteSpace(a))
-            {
-                a = "0";
-            }
-            if (string.IsNullOrWhiteSpace(b))
-            {
-                b = "0";
-            }
+            var maxDigits = Math.Max(a.Length, b.Length);
+            a = a.PadLeft(maxDigits, '0');
+            b = b.PadLeft(maxDigits, '0');
 
-            if (decimal.TryParse(a, out a_decimal) && decimal.TryParse(b, out b_decimal))
+            var res = string.Empty;
+
+            var uebertrag = 0;
+            for (int i = maxDigits - 1; i >= 0; i--)
             {
-                checked
+                var ai = char.GetNumericValue(a[i]);
+                var bi = char.GetNumericValue(b[i]);
+                var ci = ai + bi + uebertrag;
+
+                if (ci >= 10)
                 {
-                    return $"{a_decimal + b_decimal}";
+                    uebertrag = 1;
+                    ci -= 10;
                 }
+                else
+                {
+                    uebertrag = 0;
+                }
+
+                res = $"{ci}{res}";
             }
-            return string.Empty;
+            if (uebertrag == 1)
+            {
+                res = $"1{res}";
+            }
+            return res.TrimStart('0');
         }
     }
 }

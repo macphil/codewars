5ab145e461a979c3cd1a5e04b7b35feeda009501
Philip Schröder <philip.schroeder@shs-viveon.com>
2016-09-15 09:26:45 +0200

    GetReadableTime added

---
+++ codewars/GetReadableTime.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace codewars
+{
+    public class GetReadableTime
+    {
+        [Test]
+        [TestCase(0, ExpectedResult = "00:00:00")]
+        [TestCase(5, ExpectedResult = "00:00:05")]
+        [TestCase(60, ExpectedResult = "00:01:00")]
+        [TestCase(86399, ExpectedResult = "23:59:59")]
+        [TestCase(35999, ExpectedResult = "99:59:59")]
+        public string GetReadableTime_Tests(int actual)
+        {
+            return $"{actual}";
+        }
+    }
+}

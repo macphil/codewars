aeb1744a32e556d00be85a51f0325782d40ae573
Philip Schröder <philip.schroeder@shs-viveon.com>
2016-09-19 20:50:07 +0200

    BinaryNumbers added

---
+++ codewars/BinaryNumbers_Tests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace codewars
+{
+    public class BinaryNumbers_Tests
+    {
+        [Test]
+        [TestCase("111", "10", ExpectedResult = "1001")]
+        [TestCase("1101", "101", ExpectedResult = "10010")]
+        [TestCase("1101", "10111", ExpectedResult = "100100")]
+        [TestCase("00011", "00", ExpectedResult = "11")]
+        [TestCase("00", "11", ExpectedResult = "11")]
+        public string BinaryNumbers_Tests_Tests(string a, string b)
+        {
+            return Add(a, b);
+        }
+
+        public static string Add(string a, string b)
+        {
+            return "";
+        }
+    }
+}

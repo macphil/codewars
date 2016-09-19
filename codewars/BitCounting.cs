518f272f500d5bc7ed1f95ef8881b570ca0777c3
Philip Schröder <philip.schroeder@shs-viveon.com>
2016-09-19 23:07:42 +0200

    first approach

--- codewars/BitCounting.cs
+++ codewars/BitCounting.cs
@@ -16,9 +16,27 @@ namespace codewars
             return CountBits(actual);
         }
 
-        private static int CountBits(int actual)
+        private static int CountBits(int n)
         {
-            return -1;
+            if (n > byte.MaxValue || n < byte.MinValue)
+            {
+                return -1;
+            }
+            return Convert.ToString(Convert.ToByte(n), 2).Replace("0", string.Empty).Length;
+        }
+
+        [TestCase(0, ExpectedResult = "0")]
+        [TestCase(1, ExpectedResult = "1")]
+        [TestCase(2, ExpectedResult = "10")]
+        [TestCase(7, ExpectedResult = "111")]
+        public string IntToBitstring(int n)
+        {
+            byte result;
+            if (Byte.TryParse(Convert.ToString(n), out result))
+            {
+                return Convert.ToString(result, 2);
+            }
+            return "";
         }
     }
 }

12dda37d4490036c7cd586e6ba4af07c60e9bcbf
Philip Schröder <philip.schroeder@shs-viveon.com>
2016-09-19 22:38:44 +0200

    BitCounting added

---
+++ codewars/BitCounting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace codewars
+{
+    public class BitCounting
+    {
+        [Test]
+        [TestCase(0, ExpectedResult = 0)]
+        [TestCase(4, ExpectedResult = 1)]
+        [TestCase(7, ExpectedResult = 3)]
+        [TestCase(9, ExpectedResult = 2)]
+        [TestCase(10, ExpectedResult = 2)]
+        public int CountBits_Tests(int actual)
+        {
+            return CountBits(actual);
+        }
+
+        private static int CountBits(int actual)
+        {
+            return -1;
+        }
+    }
+}

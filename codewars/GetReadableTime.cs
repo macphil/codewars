9d5b1c00d79ceccc1f45f54cf137d6a95f874792
Philip Schröder <philip.schroeder@shs-viveon.com>
2016-09-19 20:45:17 +0200

    submitted

--- codewars/GetReadableTime.cs
+++ codewars/GetReadableTime.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 namespace codewars
 {
@@ -9,10 +10,19 @@ namespace codewars
         [TestCase(5, ExpectedResult = "00:00:05")]
         [TestCase(60, ExpectedResult = "00:01:00")]
         [TestCase(86399, ExpectedResult = "23:59:59")]
-        [TestCase(35999, ExpectedResult = "99:59:59")]
-        public string GetReadableTime_Tests(int actual)
-        {
-            return $"{actual}";
+        [TestCase(359999, ExpectedResult = "99:59:59")]
+        public string GetReadableTime_Tests(int seconds)
+        { /*
+            // the easy way using DateTime:
+            var dateTime = new DateTime(0).AddSeconds(seconds);
+            return $"{(Math.Floor((dateTime - new DateTime(0)).TotalHours)):00}:{dateTime:mm:ss}";
+            */
+
+            var secs = seconds % 60;
+            var mins = seconds / 60 % 60;
+            var hours = seconds / (60 * 60) % (60 * 60);
+
+            return $"{hours:D2}:{mins:D2}:{secs:D2}";
         }
     }
 }

6c5d7b143f2ce3406189650990a47dae062c1acb
Philip Schröder <philip.schroeder@shs-viveon.com>
2016-09-26 22:27:29 +0200

    first approach

--- codewars/ValidBraces .cs
+++ codewars/ValidBraces .cs
@@ -16,11 +16,55 @@ namespace codewars
         }
     }
 
+    /*
+     * All input strings will be nonempty, and will only consist of
+     * open parentheses '(' ,
+     * closed parentheses ')',
+     * open brackets '[',
+     * closed brackets ']',
+     * open curly braces '{' and
+     * closed curly braces '}'.
+     * */
+
     internal class Brace
     {
         internal static bool validBraces(string braces)
         {
-            return false;
+            var openParentheses = 0;
+            var openBrackets = 0;
+            var openCurlyBraces = 0;
+
+            foreach (char c in braces.ToCharArray())
+            {
+                switch (c)
+                {
+                    case '[':
+                        openBrackets++;
+                        break;
+
+                    case ']':
+                        openBrackets--;
+                        break;
+
+                    case '{':
+                        openCurlyBraces++;
+                        break;
+
+                    case '}':
+                        openBrackets--;
+                        break;
+
+                    case '(':
+                        openParentheses++;
+                        break;
+
+                    case ')':
+                        openParentheses--;
+                        break;
+                }
+            }
+
+            return openCurlyBraces == 0 && openBrackets == 0 && openParentheses == 0;
         }
     }
 }

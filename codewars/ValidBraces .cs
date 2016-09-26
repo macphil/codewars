48d599b566c766f63d1cdbfd703381f17d239f66
Philip Schröder <philip.schroeder@shs-viveon.com>
2016-09-26 22:19:00 +0200

    validBraces added

---
+++ codewars/ValidBraces .cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace codewars
+{
+    public class ValidBraces
+    {
+        [Test]
+        [TestCase("(){}[]", ExpectedResult = true)]
+        [TestCase("(}", ExpectedResult = false)]
+        [TestCase("[(])", ExpectedResult = false)]
+        [TestCase("([{}])", ExpectedResult = true)]
+        public bool ValidBraces_Tests(string actual)
+        {
+            return Brace.validBraces(actual);
+        }
+    }
+
+    internal class Brace
+    {
+        internal static bool validBraces(string braces)
+        {
+            return false;
+        }
+    }
+}

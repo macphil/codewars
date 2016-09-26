c252ca2bd7e2f1d534d7800963216782864569aa
Philip Schröder <philip.schroeder@shs-viveon.com>
2016-09-26 21:14:15 +0200

    DoubleChar added

---
+++ codewars/DoubleChar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace codewars
+{
+    public class DoubleChar
+    {
+        [Test]
+        [TestCase("abcd", ExpectedResult = "aabbccdd")]
+        [TestCase("Adidas", ExpectedResult = "AAddiiddaass")]
+        [TestCase("1337", ExpectedResult = "11333377")]
+        [TestCase("illuminati", ExpectedResult = "iilllluummiinnaattii")]
+        [TestCase("123456", ExpectedResult = "112233445566")]
+        [TestCase("%^&*(", ExpectedResult = "%%^^&&**((")]
+        public static string FixedTest(string s)
+        {
+            return Kata.DoubleChar(s);
+        }
+    }
+
+    internal class Kata
+    {
+        internal static string DoubleChar(string s)
+        {
+            return s;
+        }
+    }
+}

08336dccbf3a03c248756476a8dbb23a4f15e44e
Philip Schröder <philip.schroeder@shs-viveon.com>
2016-09-30 23:45:23 +0200

    ookkk-ok-o-ook-ok-ooo

---
+++ codewars/ookkk-ok-o-ook-ok-ooo.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace codewars
+{
+    public class ookkk_ok_o_ook_ok_ooo
+    {
+        [Test]
+        [TestCase("Ok, Ook, Ooo!", ExpectedResult = "H")]
+        [TestCase("Okk, Ook, Ok!", ExpectedResult = "e")]
+        [TestCase("Okk, Okk, Oo!", ExpectedResult = "l")]
+        [TestCase("Okk, Okkkk!", ExpectedResult = "o")]
+        [TestCase("Ok, Ok, Okkk!", ExpectedResult = "W")]
+        [TestCase("Okkk, Ook, O!", ExpectedResult = "r")]
+        [TestCase("Okk, Ook, Oo!", ExpectedResult = "d")]
+        [TestCase("Ok, Ook, Ooo?  Okk, Ook, Ok?  Okk, Okk, Oo?  Okk, Okk, Oo?  Okk, Okkkk!", ExpectedResult = "Hello")]
+        [TestCase("Ok, Ok, Okkk?  Okk, Okkkk?  Okkk, Ook, O?  Okk, Okk, Oo?  Okk, Ook, Oo?  Ook, Ooook!",
+            ExpectedResult = "World!")]
+        public string ookkk_ok_o_ook_ok_ooo_Tests(string actual) => okkOokOo(actual);
+
+        public static string okkOokOo(string okkOookk)
+        {
+            return string.Empty;
+        }
+    }
+}

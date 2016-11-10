40265e114e8860e0188ae5932559935b04327cbf
Philip Schröder <philip.schroeder@shs-viveon.com>
2016-11-10 22:44:03 +0100

    SevenSegmentConverter

---
+++ codewars/SevenSegmentConverter.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+
+namespace codewars
+{
+    public class SevenSegmentConverter
+    {
+        public static int SevenSegmentNumber(int number)
+        {
+            return number;
+        }
+
+        [Test]
+        [TestCase(4, ExpectedResult = 90)]
+        public int SevenSegmentConverterTests(int actual)
+        {
+            return SevenSegmentNumber(actual);
+        }
+    }
+
+    public class SevenSegment
+    {
+        private bool _top_horizontal;
+        private bool _middle_horizontal;
+        private bool _bottom_horizontal;
+        private bool _top_left_vertical;
+        private bool _top_right_vertical;
+        private bool _bottom_left_vertical;
+        private bool _bottom_right_vertical;
+
+        public SevenSegment(
+            bool bottom_right_vertical,
+            bool bottom_left_vertical,
+            bool top_right_vertical,
+            bool top_left_vertical,
+            bool bottom_horizontal,
+            bool middle_horizontal,
+            bool top_horizontal)
+        {
+            _bottom_horizontal = bottom_horizontal;
+            _bottom_left_vertical = bottom_left_vertical;
+            _bottom_right_vertical = bottom_right_vertical;
+            _middle_horizontal = middle_horizontal;
+            _top_horizontal = top_horizontal;
+            _top_left_vertical = top_left_vertical;
+            _top_right_vertical = top_right_vertical;
+        }
+    }
+}

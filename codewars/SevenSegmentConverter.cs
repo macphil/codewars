1300b3fa4b37b40d100c792c07afe897f38e4cea
Philip Schröder <philip.schroeder@shs-viveon.com>
2016-11-10 23:52:36 +0100

    SevenSegmentConverter: first approach

--- codewars/SevenSegmentConverter.cs
+++ codewars/SevenSegmentConverter.cs
@@ -1,7 +1,98 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 namespace codewars
 {
+    public enum OnOff
+    {
+        Off = 0,
+        On = 1
+    }
+
+    public static class OnOffExtensions
+    {
+        public static char X(this OnOff onOffValue) => onOffValue == OnOff.On ? '#' : ' ';
+    }
+
+    /*
+   *
+       The bits for each segment are in the following order,
+       from right to left: (Binary always reads from right to left!)
+
+       7_Top Horizontal
+       6_Middle Horizontal
+       5_Bottom Horizontal
+       4_Top-Left Vertical
+       3_Top-Right Vertical
+       2_Bottom-Left Vertical
+       1_Bottom-Right Vertical
+   */
+
+    public class SevenSegment
+    {
+        public SevenSegment Four => new SevenSegment
+        {
+            TopLeftVertical = OnOff.On,
+            MiddleHorizontal = OnOff.On,
+            TopRightVertical = OnOff.On,
+            BottomRightVertical = OnOff.On
+        };
+
+        public SevenSegment One => new SevenSegment
+        {
+            TopRightVertical = OnOff.On,
+            BottomRightVertical = OnOff.On
+        };
+
+        private OnOff BottomHorizontal { get; set; } = OnOff.Off;
+
+        private OnOff BottomLeftVertical { get; set; } = OnOff.Off;
+
+        private OnOff BottomRightVertical { get; set; } = OnOff.Off;
+
+        private OnOff MiddleHorizontal { get; set; } = OnOff.Off;
+
+        private OnOff TopHorizontal { get; set; } = OnOff.Off;
+
+        private OnOff TopLeftVertical { get; set; } = OnOff.Off;
+
+        private OnOff TopRightVertical { get; set; } = OnOff.Off;
+
+        public override string ToString()
+        {
+            var middle = new string(' ', 3);
+            var asciArt = $"" +
+                $"{new string(TopHorizontal.X(), 5)}\n" +
+                $"{TopLeftVertical.X()}{middle}{TopRightVertical.X()}\n" +
+                $"{TopLeftVertical.X()}{middle}{TopRightVertical.X()}\n" +
+                $"{new string(MiddleHorizontal.X(), 5)}\n" +
+                $"{BottomLeftVertical.X()}{middle}{BottomRightVertical.X()}\n" +
+                $"{BottomLeftVertical.X()}{middle}{BottomRightVertical.X()}\n" +
+                $"{new string(BottomHorizontal.X(), 5)}\n";
+
+            return asciArt;
+        }
+
+        private void xSevenSegment(
+            OnOff bottom_right_vertical,
+            OnOff bottom_left_vertical,
+            OnOff top_right_vertical,
+            OnOff top_left_vertical,
+            OnOff bottom_horizontal,
+            OnOff middle_horizontal,
+            OnOff top_horizontal)
+        {
+            BottomHorizontal = bottom_horizontal;
+            BottomLeftVertical = bottom_left_vertical;
+            BottomRightVertical = bottom_right_vertical;
+            MiddleHorizontal = middle_horizontal;
+            TopHorizontal = top_horizontal;
+            TopLeftVertical = top_left_vertical;
+            TopRightVertical = top_right_vertical;
+        }
+    }
+
+    // https://www.codewars.com/kata/7-segment-converter/csharp
     public class SevenSegmentConverter
     {
         public static int SevenSegmentNumber(int number)
@@ -13,36 +104,8 @@ namespace codewars
         [TestCase(4, ExpectedResult = 90)]
         public int SevenSegmentConverterTests(int actual)
         {
+            Console.WriteLine(new SevenSegment().Four.ToString());
             return SevenSegmentNumber(actual);
-        }
-    }
-
-    public class SevenSegment
-    {
-        private bool _top_horizontal;
-        private bool _middle_horizontal;
-        private bool _bottom_horizontal;
-        private bool _top_left_vertical;
-        private bool _top_right_vertical;
-        private bool _bottom_left_vertical;
-        private bool _bottom_right_vertical;
-
-        public SevenSegment(
-            bool bottom_right_vertical,
-            bool bottom_left_vertical,
-            bool top_right_vertical,
-            bool top_left_vertical,
-            bool bottom_horizontal,
-            bool middle_horizontal,
-            bool top_horizontal)
-        {
-            _bottom_horizontal = bottom_horizontal;
-            _bottom_left_vertical = bottom_left_vertical;
-            _bottom_right_vertical = bottom_right_vertical;
-            _middle_horizontal = middle_horizontal;
-            _top_horizontal = top_horizontal;
-            _top_left_vertical = top_left_vertical;
-            _top_right_vertical = top_right_vertical;
         }
     }
 }

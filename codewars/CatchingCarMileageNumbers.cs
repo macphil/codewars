bc925122a896be6b3cb6073dd9710a85c6d303b1
Philip Schröder <philip.schroeder@shs-viveon.com>
2017-02-09 23:08:14 +0100

    Catching Car Mileage Numbers

---
+++ codewars/CatchingCarMileageNumbers.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace codewars
+{
+    public class CatchingCarMileageNumbers
+    {
+        [Test]
+        [TestCase("", "")]
+        public void CatchingCarMileageNumbers_AreEqual(string actual, string expected)
+        {
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [TestCase("", ExpectedResult = "")]
+        public string CatchingCarMileageNumbers_Tests(string actual)
+        {
+            return actual;
+        }
+    }
+}

81ae58de4bbaf34a3b6376dc87f2a9cde0aef040
Philip Schröder <philip.schroeder@shs-viveon.com>
2017-02-09 23:12:55 +0100

    unitTest added

--- codewars/CatchingCarMileageNumbers.cs
+++ codewars/CatchingCarMileageNumbers.cs
@@ -1,14 +1,13 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace codewars
 {
     public class CatchingCarMileageNumbers
     {
-        [Test]
-        [TestCase("", "")]
-        public void CatchingCarMileageNumbers_AreEqual(string actual, string expected)
+        public static int IsInteresting(int number, List<int> awesomePhrases)
         {
-            Assert.AreEqual(expected, actual);
+            return -1;
         }
 
         [Test]
@@ -16,6 +15,17 @@ namespace codewars
         public string CatchingCarMileageNumbers_Tests(string actual)
         {
             return actual;
+        }
+
+        [Test]
+        public void ShouldWorkTest()
+        {
+            Assert.AreEqual(0, IsInteresting(3, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(1, IsInteresting(1336, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(2, IsInteresting(1337, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(0, IsInteresting(11208, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(1, IsInteresting(11209, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(2, IsInteresting(11211, new List<int>() { 1337, 256 }));
         }
     }
 }

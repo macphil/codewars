f62d238bf36dc10f03a6f002473170ab7bdd0202
Philip Schröder <philip.schroeder@shs-viveon.com>
2017-02-09 22:49:41 +0100

    abort

---
+++ codewars/7Segment/SevenSegment.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace codewars._7Segment
+{
+    public enum SevenSegmentLED
+    {
+        TopHorizontal = 1,
+        MiddleHorizontal = 2,
+        BottomHorizontal = 4,
+        TopLeftVertical = 8,
+        TopRightVertical = 16,
+        BottomLeftVertical = 32,
+        BottomRightVertical = 64
+    }
+
+    public class SevenSegment
+    {
+        private readonly List<SevenSegmentLED> _activeSegments;
+
+        public SevenSegment(List<SevenSegmentLED> activeSegments)
+        {
+            _activeSegments = activeSegments;
+        }
+
+        public static SevenSegment Nr0 => new SevenSegment(new List<SevenSegmentLED>
+        {SevenSegmentLED.TopHorizontal, SevenSegmentLED.TopRightVertical, SevenSegmentLED.BottomRightVertical, SevenSegmentLED.BottomHorizontal, SevenSegmentLED.BottomLeftVertical, SevenSegmentLED.TopLeftVertical});
+
+        public static SevenSegment Nr1 => new SevenSegment(new List<SevenSegmentLED>
+        {SevenSegmentLED.TopRightVertical, SevenSegmentLED.BottomRightVertical });
+
+        public static SevenSegment Nr2 => new SevenSegment(new List<SevenSegmentLED>
+        {SevenSegmentLED.TopHorizontal, SevenSegmentLED.TopRightVertical, SevenSegmentLED.MiddleHorizontal, SevenSegmentLED.BottomLeftVertical, SevenSegmentLED.BottomHorizontal });
+
+        public static SevenSegment Nr3 => new SevenSegment(new List<SevenSegmentLED>
+        {SevenSegmentLED.TopHorizontal, SevenSegmentLED.TopRightVertical, SevenSegmentLED.MiddleHorizontal, SevenSegmentLED.BottomRightVertical, SevenSegmentLED.BottomHorizontal });
+
+        public static SevenSegment Nr4 => new SevenSegment(new List<SevenSegmentLED>
+        {SevenSegmentLED.TopLeftVertical, SevenSegmentLED.MiddleHorizontal, SevenSegmentLED.TopRightVertical, SevenSegmentLED.BottomRightVertical });
+
+        public static SevenSegment Nr5 => new SevenSegment(new List<SevenSegmentLED>
+        {SevenSegmentLED.TopHorizontal, SevenSegmentLED.TopLeftVertical, SevenSegmentLED.MiddleHorizontal, SevenSegmentLED.BottomRightVertical, SevenSegmentLED.BottomHorizontal});
+
+        public static SevenSegment Nr6 => new SevenSegment(new List<SevenSegmentLED>
+        {SevenSegmentLED.TopHorizontal, SevenSegmentLED.TopLeftVertical, SevenSegmentLED.MiddleHorizontal, SevenSegmentLED.BottomRightVertical, SevenSegmentLED.BottomHorizontal, SevenSegmentLED.BottomLeftVertical});
+
+        public static SevenSegment Nr7 => new SevenSegment(new List<SevenSegmentLED>
+        {SevenSegmentLED.TopHorizontal, SevenSegmentLED.TopRightVertical, SevenSegmentLED.BottomRightVertical });
+
+        public static SevenSegment Nr8 => new SevenSegment(new List<SevenSegmentLED>
+        {SevenSegmentLED.TopHorizontal, SevenSegmentLED.TopRightVertical, SevenSegmentLED.BottomRightVertical, SevenSegmentLED.BottomHorizontal, SevenSegmentLED.BottomLeftVertical, SevenSegmentLED.TopLeftVertical, SevenSegmentLED.MiddleHorizontal});
+
+        public static SevenSegment Nr9 => new SevenSegment(new List<SevenSegmentLED>
+        {SevenSegmentLED.TopHorizontal, SevenSegmentLED.TopRightVertical, SevenSegmentLED.MiddleHorizontal, SevenSegmentLED.TopLeftVertical, SevenSegmentLED.BottomRightVertical, SevenSegmentLED.BottomHorizontal});
+
+        public override string ToString()
+        {
+            var asciiArt = new StringBuilder();
+            var o = new string(' ', 3);
+
+            asciiArt.Append(GetChar(SevenSegmentLED.TopLeftVertical));
+            asciiArt.Append(new string(GetChar(SevenSegmentLED.TopHorizontal), 3));
+            asciiArt.Append(GetChar(SevenSegmentLED.TopRightVertical));
+            asciiArt.AppendLine();
+
+            asciiArt.Append(GetChar(SevenSegmentLED.TopLeftVertical));
+            asciiArt.Append(o);
+            asciiArt.Append(GetChar(SevenSegmentLED.TopRightVertical));
+            asciiArt.AppendLine();
+
+            asciiArt.Append(GetChar(SevenSegmentLED.TopLeftVertical));
+            asciiArt.Append(o);
+            asciiArt.Append(GetChar(SevenSegmentLED.TopRightVertical));
+            asciiArt.AppendLine();
+
+            asciiArt.Append(GetChar(SevenSegmentLED.BottomLeftVertical));
+            asciiArt.Append(new string(GetChar(SevenSegmentLED.MiddleHorizontal), 3));
+            asciiArt.Append(GetChar(SevenSegmentLED.BottomRightVertical));
+            asciiArt.AppendLine();
+
+            asciiArt.Append(GetChar(SevenSegmentLED.BottomLeftVertical));
+            asciiArt.Append(o);
+            asciiArt.Append(GetChar(SevenSegmentLED.BottomRightVertical));
+            asciiArt.AppendLine();
+
+            asciiArt.Append(GetChar(SevenSegmentLED.BottomLeftVertical));
+            asciiArt.Append(o);
+            asciiArt.Append(GetChar(SevenSegmentLED.BottomRightVertical));
+            asciiArt.AppendLine();
+
+            asciiArt.AppendLine(new string(GetChar(SevenSegmentLED.BottomHorizontal), 5));
+
+            return asciiArt.ToString();
+        }
+
+        public int Value()
+        {
+            Console.WriteLine(this.ToString());
+            return _activeSegments.Sum(x => (int)x);
+        }
+
+        private char GetChar(SevenSegmentLED led)
+        {
+            return _activeSegments.Contains(led) ? '#' : ' ';
+        }
+    }
+}
---
+++ codewars/7Segment/SevenSegmentConverter.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using codewars._7Segment;
+using NUnit.Framework;
+
+namespace codewars
+{
+    // https://www.codewars.com/kata/7-segment-converter/csharp
+    public class SevenSegmentConverter
+    {
+        public static int SevenSegmentNumber(int number)
+        {
+            switch (number)
+            {
+                case 0:
+                    return SevenSegment.Nr0.Value();
+
+                case 1:
+                    return SevenSegment.Nr1.Value();
+
+                case 2:
+                    return SevenSegment.Nr2.Value();
+
+                case 3:
+                    return SevenSegment.Nr3.Value();
+
+                case 4:
+                    return SevenSegment.Nr4.Value();
+
+                case 5:
+                    return SevenSegment.Nr5.Value();
+
+                case 6:
+                    return SevenSegment.Nr6.Value();
+
+                case 7:
+                    return SevenSegment.Nr7.Value();
+
+                case 8:
+                    return SevenSegment.Nr8.Value();
+
+                case 9:
+                    return SevenSegment.Nr9.Value();
+
+                default:
+                    throw new ArgumentException($"Number '{number}' is out of range");
+            }
+        }
+
+        [Test]
+        public void AllNr()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i}:");
+                SevenSegmentNumber(i);
+            }
+        }
+
+        [Test]
+        [TestCase(0, ExpectedResult = 125)]
+        [TestCase(1, ExpectedResult = 80)]
+        [TestCase(2, ExpectedResult = 55)]
+        [TestCase(3, ExpectedResult = 87)]
+        [TestCase(4, ExpectedResult = 90)]
+        [TestCase(5, ExpectedResult = 79)]
+        [TestCase(6, ExpectedResult = 111)]
+        [TestCase(7, ExpectedResult = 81)]
+        [TestCase(8, ExpectedResult = 127)]
+        [TestCase(9, ExpectedResult = 91)]
+        [TestCase(10, ExpectedResult = -1)]
+        public int SevenSegmentConverterTests(int actual)
+        {
+            return SevenSegmentNumber(actual);
+        }
+    }
+}

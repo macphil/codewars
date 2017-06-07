f4d1b4663125e4faeca7b869320de476c9da3c43
Philip Schröder <philip.schroeder@shs-viveon.com>
2017-06-07 15:59:53 +0200

    tdd validate

--- codewars/DomainNameValidatorTests.cs
+++ codewars/DomainNameValidatorTests.cs
@@ -12,13 +12,15 @@ namespace codewars
         [TestCase("codewars", ExpectedResult = false)]
         [TestCase(".codewars.com", ExpectedResult = false)]
         [TestCase("127.0.0.1", ExpectedResult = false)]
+        [TestCase("L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L", ExpectedResult = false)]
+        [TestCase("L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L.L", ExpectedResult = true)]
         [TestCase("codewars.com-", ExpectedResult = false)]
         [TestCase("example@codewars.com", ExpectedResult = false)]
         [TestCase("CODEWARS.COM", ExpectedResult = true)]
         [TestCase("codewars.com", ExpectedResult = true)]
         [TestCase("g.co", ExpectedResult = true)]
         [TestCase("sub.codewars.com", ExpectedResult = true)]
-        public string DomainNameValidator_Tests(string actual)
+        public bool DomainNameValidator_Tests(string actual)
         {
             return v.validate(actual);
         }
@@ -32,9 +34,42 @@ namespace codewars
 
     internal class DomainNameValidator
     {
-        internal string validate(string actual)
+        /***
+         * In this kata you have to create a domain name validator mostly compliant with RFC 1035, RFC 1123, and RFC 2181
+
+        For purposes of this kata, following rules apply:
+
+            Domain name may contain subdomains (levels), hierarchically separated by . (period) character
+            Domain name must not contain more than 127 levels, including top level (TLD)
+            Domain name must not be longer than 253 characters (RFC specifies 255, but 2 characters are reserved for trailing dot and null character for root level)
+            Level names must be composed out of lowercase and uppercase ASCII letters, digits and - (minus sign) character
+            Level names must not start or end with - (minus sign) character
+            Level names must not be longer than 63 characters
+            Top level (TLD) must not be fully numerical
+
+        Additionally, in this kata
+
+            Domain name must contain at least one subdomain (level) apart from TLD
+            Top level validation must be naive - ie. TLDs nonexistent in IANA register are still considered valid as long as they adhere to the rules given above.
+
+        The validation function accepts string with the full domain name and returns boolean value indicating whether the domain name is valid or not.
+        */
+
+        public bool validate(string domain)
         {
-            throw new NotImplementedException();
+            if (!ValidateDomainLength(domain)) return false;
+            var levels = domain.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+            if (levels.Length < 2 || levels.Length > 127)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool ValidateDomainLength(string domain)
+        {
+            return !string.IsNullOrWhiteSpace(domain) && domain.Trim().Length <= 253;
         }
     }
 }

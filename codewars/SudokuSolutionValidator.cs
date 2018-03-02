ff0690d5e05a8eb14cd247874187c4cac40fd93c
Philip Schröder <Philip.Schroeder@SHS-VIVEON.com>
2018-03-02 17:29:04 +0100

    ValidateBlocks (first approach with switch)

--- codewars/SudokuSolutionValidator.cs
+++ codewars/SudokuSolutionValidator.cs
@@ -17,7 +17,92 @@ namespace codewars
     {
         public static bool ValidateSolution(int[][] board)
         {
-            return ValidateRows(board) && ValidateColumns(board);
+            return ValidateRows(board) && ValidateColumns(board) && ValidateBlocks(board);
+        }
+
+        internal static bool ValidateBlocks(int[][] board)
+        {
+            for (int i = 0; i < board.Length; i++)
+            {
+                var sequenceToTest = GetBlockSequence(board, i);
+                if (!ValidateSequence(sequenceToTest))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static int[] GetBlockSequence(int[][] board, int block)
+        {
+            int x = 0;
+            int y = 0;
+            switch (block)
+            {
+                case 0:
+                    x = 0;
+                    y = 0;
+                    break;
+
+                case 1:
+                    x = 3;
+                    y = 0;
+                    break;
+
+                case 2:
+                    x = 6;
+                    y = 0;
+                    break;
+
+                case 3:
+                    x = 0;
+                    y = 3;
+                    break;
+
+                case 4:
+                    x = 3;
+                    y = 3;
+                    break;
+
+                case 5:
+                    x = 6;
+                    y = 3;
+                    break;
+
+                case 6:
+                    x = 0;
+                    y = 6;
+                    break;
+
+                case 7:
+                    x = 3;
+                    y = 6;
+                    break;
+
+                case 8:
+                    x = 6;
+                    y = 6;
+                    break;
+
+                default:
+                    x = 0;
+                    y = 0;
+                    break;
+            }
+
+            var blockWidth = (int)Math.Floor(Math.Sqrt(board.Length));
+            var sequenceToTest = new int[board.Length];
+            int pos = 0;
+
+            for (int i = 0; i < blockWidth; i++)
+            {
+                for (int j = 0; j < blockWidth; j++)
+                {
+                    sequenceToTest[pos++] = board[x + i][y + j];
+                }
+            }
+
+            return sequenceToTest;
         }
 
         internal static bool ValidateColumns(int[][] board)
@@ -102,6 +187,22 @@ namespace codewars
                     new int[] {3, 4, 5, 2, 8, 6, 1, 7, 9},
                 },
             },
+            new object[]
+            {
+                false,
+                new int[][]
+                {
+                    new int[] { 7, 8, 9, 1, 2, 3, 4, 5, 6},
+                    new int[] { 8, 9, 7, 2, 3, 1, 5, 6, 4},
+                    new int[] { 9, 7, 8, 3, 1, 2, 6, 4, 5},
+                    new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9},
+                    new int[] { 2, 3, 1, 5, 6, 4, 8, 9, 7},
+                    new int[] { 3, 1, 2, 6, 4, 5, 9, 7, 8},
+                    new int[] { 4, 5, 6, 7, 8, 9, 1, 2, 3},
+                    new int[] { 5, 6, 4, 8, 9, 7, 2, 3, 1},
+                    new int[] { 6, 4, 5, 9, 7, 8, 3, 1, 2},
+                },
+            },
         };
 
         public static IEnumerable TestSequences
@@ -123,5 +224,29 @@ namespace codewars
         [Test]
         [TestCaseSource(nameof(TestSequences))]
         public bool TestValidateSequence(int[] sequenceToTest) => Sudoku.ValidateSequence(sequenceToTest);
+
+        [Test]
+        public void TestValidateBlocks()
+        {
+            // arrange
+            var board = new int[][]
+            {
+                new int[] {1, 2, 3, 1, 2, 3, 1, 2, 3},
+                new int[] {4, 5, 6, 4, 5, 6, 4, 5, 6},
+                new int[] {7, 8, 9, 7, 8, 9, 7, 8, 9},
+                new int[] {1, 2, 3, 1, 2, 3, 1, 2, 3},
+                new int[] {4, 5, 6, 4, 5, 6, 4, 5, 6},
+                new int[] {7, 8, 9, 7, 8, 9, 7, 8, 9},
+                new int[] {1, 2, 3, 1, 2, 3, 1, 2, 3},
+                new int[] {4, 5, 6, 4, 5, 6, 4, 5, 6},
+                new int[] {7, 8, 9, 7, 8, 9, 7, 8, 8},
+            };
+
+            // act
+            bool isValid = Sudoku.ValidateBlocks(board);
+
+            // assert
+            Assert.That(isValid, Is.False);
+        }
     }
 }

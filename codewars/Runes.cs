af83c621571185e121fbd80943dda2e961fe3f23
Philip Schröder <Philip.Schroeder@SHS-VIVEON.com>
2018-03-27 18:09:39 +0200

    add SolveExpressionTests

--- codewars/Runes.cs
+++ codewars/Runes.cs
@@ -19,7 +19,18 @@ public class Runes
 [TestFixture]
 public class RunesTest
 {
+    // regex: s: .*\(([^,]*), Runes\.solveExpression\(([^\)]*)\), ([^\)]*).* r: [TestCase($2, ExpectedResult = $1, TestName = $3)]
     [Test]
+    [TestCase("1+1=?", ExpectedResult = 2, TestName = "Answer for expression '1+1=?' ")]
+    [TestCase("123*45?=5?088", ExpectedResult = 6, TestName = "Answer for expression '123*45?=5?088' ")]
+    [TestCase("-5?*-1=5?", ExpectedResult = 0, TestName = "Answer for expression '-5?*-1=5?' ")]
+    [TestCase("19--45=5?", ExpectedResult = -1, TestName = "Answer for expression '19--45=5?' ")]
+    [TestCase("??*??=302?", ExpectedResult = 5, TestName = "Answer for expression '??*??=302?' ")]
+    [TestCase("?*11=??", ExpectedResult = 2, TestName = "Answer for expression '?*11=??' ")]
+    [TestCase("??*1=??", ExpectedResult = 2, TestName = "Answer for expression '??*1=??' ")]
+    public int SolveExpressionTests(string expression) => Runes.solveExpression(expression);
+
+    [Test, Ignore("not yet finished")]
     public void testSample()
     {
         Assert.AreEqual(2, Runes.solveExpression("1+1=?"), "Answer for expression '1+1=?' ");

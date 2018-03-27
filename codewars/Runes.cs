11d95c0d4893bb8d1186346ef0c836575ef0469d
Philip Schröder <Philip.Schroeder@SHS-VIVEON.com>
2018-03-27 17:51:16 +0200

    add https://www.codewars.com/kata/find-the-unknown-digit

---
+++ codewars/Runes.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+// see https://www.codewars.com/kata/find-the-unknown-digit
+public class Runes
+{
+    public static int solveExpression(string expression)
+    {
+        int missingDigit = -1;
+
+        //Write code to determine the missing digit or unknown rune
+        //Expression will always be in the form
+        //(number)[opperator](number)=(number)
+        //Unknown digit will not be the same as any other digits used in expression
+
+        return missingDigit;
+    }
+}
+
+[TestFixture]
+public class RunesTest
+{
+    [Test]
+    public void testSample()
+    {
+        Assert.AreEqual(2, Runes.solveExpression("1+1=?"), "Answer for expression '1+1=?' ");
+        Assert.AreEqual(6, Runes.solveExpression("123*45?=5?088"), "Answer for expression '123*45?=5?088' ");
+        Assert.AreEqual(0, Runes.solveExpression("-5?*-1=5?"), "Answer for expression '-5?*-1=5?' ");
+        Assert.AreEqual(-1, Runes.solveExpression("19--45=5?"), "Answer for expression '19--45=5?' ");
+        Assert.AreEqual(5, Runes.solveExpression("??*??=302?"), "Answer for expression '??*??=302?' ");
+        Assert.AreEqual(2, Runes.solveExpression("?*11=??"), "Answer for expression '?*11=??' ");
+        Assert.AreEqual(2, Runes.solveExpression("??*1=??"), "Answer for expression '??*1=??' ");
+    }
+}

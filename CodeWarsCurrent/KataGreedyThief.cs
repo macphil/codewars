6fb328567a8b58a0a6a9199f1a8cfd7e0365585d
Philip Schröder <post@macphil.de>
2018-05-08 20:40:09 +0200

    start greedythief

---
+++ CodeWarsCurrent/KataGreedyThief.cs
@@ -0,0 +1,23 @@
+﻿// see https://www.codewars.com/kata/greedy-thief
+
+namespace codewars
+{
+    using System.Collections.Generic;
+    public class Item
+    {
+        public int Weight { get; set; }
+
+        public int Price { get; set; }
+
+        public string ItemName { get; set; }
+    }
+
+
+    public class KataGreedyThief
+    {
+        public static List<Item> GreedyThief(List<Item> potentialItems, int maxWeight)
+        {
+            return new List<Item>();
+        }
+    }
+}
